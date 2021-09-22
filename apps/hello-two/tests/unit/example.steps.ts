362896856b3aae5e1a6b30abd40e1d9dd7608d70
unknown <tania.simpson@gmail.com>
2021-09-22 12:47:12 -0500

    more tests

---
+++ apps/hello-two/tests/unit/example.steps.ts
@@ -0,0 +1,24 @@
+/* eslint-disable prettier/prettier */
+import { defineFeature, loadFeature } from "jest-cucumber";
+import { shallowMount, VueWrapper } from "@vue/test-utils";
+import HelloWorld from "@/components/HelloWorld.vue";
+
+const feature = loadFeature("features/example.feature", { loadRelativePath : true });
+
+defineFeature(feature, (test) => {
+  test("Opening the initial web page", ({ given, when, then }) => {
+    let wrapper: VueWrapper<any>;
+
+    given("The page is open in a browser", () => {
+      wrapper = shallowMount(HelloWorld);
+    });
+
+    when("I inspect the page", () => {
+      //Nothing here just need it
+    });
+
+    then("I should see the landing page", () => {
+      expect(wrapper.html()).toMatch(/^<div.*/);
+    });
+  });
+});
