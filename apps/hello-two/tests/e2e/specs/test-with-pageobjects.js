32dfb7e61a0c44cdbca90cd9d9631de96086ceb8
unknown <tania.simpson@gmail.com>
2021-10-08 09:09:07 -0500

    e2e

--- apps/hello-two/tests/e2e/specs/test-with-pageobjects.js
+++ apps/hello-two/tests/e2e/specs/test-with-pageobjects.js
@@ -20,11 +20,4 @@ module.exports = {
 
     browser.end()
   },
-
-  'verify if string "e2e-nightwatch" is within the cli plugin links': (browser) => {
-    const homepage = browser.page.homepage()
-    const welcomeSection = homepage.section.app.section.welcome
-
-    welcomeSection.expect.element('@cliPluginLinks').text.to.contain('e2e-nightwatch')
-  }
 }
